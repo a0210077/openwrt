ce25793af73a2df4543413ba6f019f54ee15dd01
a0210077 <a0210077@gmail.com>
2022-11-09 17:27:41 +0800

    增加补充插件

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git myself https://github.com/a0210077/openwrt-package.git' >>feeds.conf.default
