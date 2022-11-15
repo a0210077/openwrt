e2918888ec25a55c7ee0d31b651952e7534449c6
盖饭 <a0210077@qq.com>
2022-11-15 16:38:18 +0800

    调整插件库

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git myself https://github.com/a0210077/openwrt-package.git' >>feeds.conf.default
